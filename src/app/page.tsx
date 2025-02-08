b5c2729029d3fa4b94f384b24519b93cdf51e0f9
Sudan Basyal <sudanbasyal74@gmail.com>
2025-02-08 20:17:39 +0545

    setting up a default main branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
-import GSAPScrollTrigger from "@/animations/GSAPScrollTrigger";
-import GSAPStarter from "@/animations/GSAPStarter";
-
 export default function Home() {
   // use
 
-  return (
-    <>
-      {/* GSAP and React Starter */}
-      {/* <GSAPStarter /> */}
-
-      {/* GSAP Scroll Trigger */}
-      <GSAPScrollTrigger />
-    </>
-  );
+  return <div>Learn GSAP</div>;
 }
